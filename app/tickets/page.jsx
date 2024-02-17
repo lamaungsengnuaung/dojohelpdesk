6ef74907a6b3b9f9df3a3f2ef5ac447859594037
lamaungSengnuaung <sengnuaung.j7core@gmail.com>
2024-02-18 02:01:24 +0630

    Fetching & Revalidating Data

---
+++ app/tickets/page.jsx
@@ -0,0 +1,17 @@
+import TicketList from './TicketList';
+
+export default function Tickes() {
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickes</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <TicketList />
+    </main>
+  );
+}
